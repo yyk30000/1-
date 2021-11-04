bbf43b187776ea8a71ae4086c2e9a9c32daeae2a
yyk30000 <49175884+yyk30000@users.noreply.github.com>
2021-11-04 10:56:18 +0900

    sql문 추가작성

---
+++ 프로젝트산출문서/0. 개발/db작업/sqlcode/데이터출력/데이터출력_쿼리.sql
@@ -0,0 +1,24 @@
+-- 검색리스트 뷰 (요리명)
+select rb.rcp_code,rcp_name,rb.rcp_decs,rb.rcp_t,rb.rcp_calory,rb.rcp_size,rcp_level,rcp_pic1
+from recipe_basics rb
+where rb.rcp_name like("%비빔밥%");
+
+
+-- 기본 정보 뷰 rcp_decs 컬럼명 수정후 쿼리문 수정 필요
+select rb.rcp_code,rcp_name,rb.rcp_decs,rb.rcp_t,rb.rcp_calory,rb.rcp_size,rcp_level,rcp_pic1
+from recipe_basics rb
+where rb.rcp_code = 90818;
+-- 재료 뷰
+select ri.rcp_code ,ri.ingre_name,ri.ingre_weight,ri.ingre_t_name 
+from recipe_ingredients ri
+where ri.rcp_code = 90818;
+
+-- 조리과정뷰
+select rc.rcp_code,rc.cooking_order,rc.cooking_content,rc.cooking_img1,rc.cooking_tip
+from recipe_cookings rc
+where rc.rcp_code = 90818 order by rc.cooking_order;
+
+-- 통합뷰 -대체식재료
+select ri.rcp_code, ri.ingre_name, ai.alter_ingre_name,ai.brief_desc
+from alternative_ingredients_link al , recipe_ingredients ri, alternative_ingredients ai
+where al.ingre_code = ri.ingre_code and ai.alt_ingre_code = al.alt_ingre_code;
