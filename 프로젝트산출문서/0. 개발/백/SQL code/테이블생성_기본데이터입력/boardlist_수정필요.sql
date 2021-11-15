bd36be9ba2ae771e3cc198380da5fc47c2f26407
yyk30000 <49175884+yyk30000@users.noreply.github.com>
2021-11-15 17:09:16 +0900

    ㄴㄴㄴ

--- 프로젝트산출문서/0. 개발/백/SQL code/테이블생성_기본데이터입력/boardlist_수정필요.sql
+++ 프로젝트산출문서/0. 개발/백/SQL code/테이블생성_기본데이터입력/boardlist_수정필요.sql
@@ -10,20 +10,26 @@ drop table boardlist;
 
 CREATE TABLE boardlist
 (
-    `img_code`      INT UNSIGNED     NOT NULL    auto_increment COMMENT '이미지 코드', 
-    `title` 		VARCHAR(50)	     NOT NULL    COMMENT '제목', 
-    `m_id`      	VARCHAR(20)      NOT NULL    COMMENT '작성자', 
-    `img_1`      	VARCHAR(30)    	 NOT NULL    COMMENT '이미지1',
-    `desc_1`        VARCHAR(2000)    NOT NULL    COMMENT '내용1',
-    `img_2`      	VARCHAR(30)      NULL    COMMENT '이미지2', 
-    `desc_2`        VARCHAR(2000)    NULL    COMMENT '내용2',
-	`img_3`      	VARCHAR(30)      NULL    COMMENT '이미지3', 
-    `desc_3`        VARCHAR(2000)    NULL    COMMENT '내용3',
-    `img_4`      	VARCHAR(30)      NULL    COMMENT '이미지4', 
-    `desc_4`        VARCHAR(2000)    NULL    COMMENT '내용4',
-	`img_5`      	VARCHAR(30)      NULL    COMMENT '이미지5', 
-    `desc_5`        VARCHAR(2000)    NULL    COMMENT '내용5',
-    `img_6`      	VARCHAR(30)      NULL    COMMENT '이미지6', 
-    `desc_6`        VARCHAR(2000)    NULL    COMMENT '내용6',
+    `img_code`  INT UNSIGNED     NOT NULL    AUTO_INCREMENT COMMENT '게시판 코드', 
+    `title`     VARCHAR(150)     NOT NULL    COMMENT '글 제목', 
+    `m_id`      VARCHAR(20)      NOT NULL    COMMENT '회원 아이디', 
+    `img_1`     VARCHAR(150)     NULL        COMMENT '이미지1', 
+    `desc_1`    VARCHAR(2000)    NOT NULL    COMMENT '내용1', 
+    `img_2`     VARCHAR(150)     NULL        COMMENT '이미지2', 
+    `desc_2`    VARCHAR(2000)    NULL        COMMENT '내용2', 
+    `img_3`     VARCHAR(150)     NULL        COMMENT '이미지3', 
+    `desc_3`    VARCHAR(2000)    NULL        COMMENT '내용3', 
+    `img_4`     VARCHAR(150)     NULL        COMMENT '이미지4', 
+    `desc_4`    VARCHAR(2000)    NULL        COMMENT '내용4', 
+    `img_5`     VARCHAR(150)     NULL        COMMENT '이미지5', 
+    `desc_5`    VARCHAR(2000)    NULL        COMMENT '내용5', 
+    `img_6`     VARCHAR(150)     NULL        COMMENT '이미지6', 
+    `desc_6`    VARCHAR(2000)    NULL        COMMENT '내용6', 
      PRIMARY KEY (img_code)
 );
+
+ALTER TABLE boardlist COMMENT '게시판 정보';
+
+ALTER TABLE boardlist
+    ADD CONSTRAINT FK_boardlist_m_id_members_m_id FOREIGN KEY (m_id)
+        REFERENCES members (m_id) ON DELETE RESTRICT ON UPDATE RESTRICT;
